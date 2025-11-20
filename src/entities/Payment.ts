49619dba9b5d8cae1a50a07635ac986f78e5553d
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2025-11-20 03:29:58 -0500

    payment entity created

---
+++ src/entities/Payment.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Appointment } from "./Appointment";
+
+@Entity("payment")
+export class Patment{
+
+    @PrimaryGeneratedColumn()
+    payment_id!: number;
+
+    @ManyToOne(() => Appointment, { onDelete: "CASCADE" })
+    @JoinColumn({ name: "appointment_id" })
+    appointment!: Appointment;
+
+    @Column({ type: "decimal", precision: 10, scale: 2 })
+    amount!: string;
+
+    @Column({ length: 50 })
+    payment_method!: string;
+
+    @Column({ length: 50, default: "pending" })
+    payment_status!: string;
+
+    @Column({ type: "json", nullable: true })
+    payment_meta?: any; // Store provider response (e.g., PayPal order id)
+
+    @CreateDateColumn({ type: "timestamp" })
+    created_at!: Date;
+}
