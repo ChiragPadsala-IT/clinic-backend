81f2c667c79b1b33e0330b7683ca93445bf8d297
ChiragPadsala-IT <chiragpadsala.it@gmail.com>
2025-11-24 02:23:16 -0500

    create main entry point of server

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import express, {Request,Response } from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { AppDataSource } from "./config/database";
+import path from 'path';
+import authRoutes from './routes/auth.routes';
+import appointmentRoutes from './routes/appointment.routes';
+import patientRoutes from './routes/patient.routes';
+import paymentRoutes from './routes/payment.routes';
+import serviceRoutes from './routes/service.routes';
 
 dotenv.config();
 
@@ -10,7 +16,29 @@ const PORT = process.env.PORT || 4000;
 
 async function start() {
     try {
+        await AppDataSource.initialize();
+        console.log("Database connected");
+
+        const app = express();
+
+        app.use(cors());
+        app.use(express.json());
+        app.use(express.urlencoded({ extended: true }));
         
+        // static upload
+        app.use("/uploads", express.static(path.join(__dirname, "..", process.env.UPLOAD_DIR || "uploads")));
+
+        //routes
+        app.use("/api/auth", authRoutes);
+        app.use("/api/patients", patientRoutes);
+        // app.use("/api/therapists", therapistsRoutes);
+        app.use("/api/services", serviceRoutes);
+        app.use("/api/appointments", appointmentRoutes);
+        app.use("/api/payments", paymentRoutes);
+
+        app.get("/", (_req: Request, res: Response) => {
+            res.json({ok: true, message: "Physio Clinic API"});
+        })
     } catch (error) {
         
     }
